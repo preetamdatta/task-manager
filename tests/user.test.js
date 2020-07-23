251fcf0ad30c9dc4618eec3820c67b7f25aff8ce
Preetam Datta <PDatta@ATD-US.COM>
2020-07-23 13:05:27 +0530

    Added jest teardown and login test cases

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,5 +1,16 @@
 const request = require('supertest')
 const app = require('../src/app')
+const User = require('../src/models/user')
+
+beforeEach(async () => {
+    await User.deleteMany()
+    const user = new User({
+        name: 'testuser1',
+        email: 'test1@mail.com',
+        password: 'testpass123'
+    })
+    await user.save()
+})
 
 test('User creation test', async () => {
     await request(app).post('/users/register').send({
@@ -7,4 +18,18 @@ test('User creation test', async () => {
         email: 'test@mail.com',
         password: 'testpass123'
     }).expect(201)
+})
+
+test('Should login existing user', async() => {
+    await request(app).post('/users/login').send({
+        email: 'test1@mail.com',
+        password: 'testpass123'
+    }).expect(200)
+})
+
+test('Should not login non-existing user', async() => {
+    await request(app).post('/users/login').send({
+        email: 'test12@mail.com',
+        password: 'testpass1234'
+    }).expect(400)
 })
